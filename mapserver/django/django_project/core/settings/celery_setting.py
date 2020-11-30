7b884b6a98c21ca57c0e88dff7a16214caf0e86d
Rizky Maulana Nugraha <lana.pcfre@gmail.com>
2020-11-30 14:57:49 +0700

    Add celery schedule

--- mapserver/django/django_project/core/settings/celery_setting.py
+++ mapserver/django/django_project/core/settings/celery_setting.py
@@ -9,11 +9,55 @@ SCHEDULE_HAZARD_EVENT_QUEUE = SCHEDULE_HAZARD_EVENT_QUEUE or {
     'minute': '*'
 }
 
+SCHEDULE_INGEST_BOUNDARY_DATA = ast.literal_eval(os.environ.get(
+    'SCHEDULE_INGEST_BOUNDARY_DATA', '{}'
+))
+SCHEDULE_INGEST_BOUNDARY_DATA = SCHEDULE_INGEST_BOUNDARY_DATA or {
+    'day_of_week': '*'
+}
+
+SCHEDULE_INGEST_BUILDING_DATA = ast.literal_eval(os.environ.get(
+    'SCHEDULE_INGEST_BUILDING_DATA', '{}'
+))
+SCHEDULE_INGEST_BUILDING_DATA = SCHEDULE_INGEST_BUILDING_DATA or {
+    'day_of_week': '*'
+}
+
+SCHEDULE_INGEST_ROAD_DATA = ast.literal_eval(os.environ.get(
+    'SCHEDULE_INGEST_ROAD_DATA', '{}'
+))
+SCHEDULE_INGEST_ROAD_DATA = SCHEDULE_INGEST_ROAD_DATA or {
+    'day_of_week': '*'
+}
+
+SCHEDULE_INGEST_HAZARD_DATA = ast.literal_eval(os.environ.get(
+    'SCHEDULE_INGEST_HAZARD_DATA', '{}'
+))
+SCHEDULE_INGEST_HAZARD_DATA = SCHEDULE_INGEST_HAZARD_DATA or {
+    'day_of_week': '*'
+}
+
 CELERYBEAT_SCHEDULE = {
     'hazard_event_queue': {
         'task': 'fba.process_hazard_event_queue',
         'schedule': crontab(**SCHEDULE_HAZARD_EVENT_QUEUE),
     },
+    'ingest_boundary_data': {
+        'task': 'fba.tasks.ingest_boundary_data',
+        'schedule': crontab(**SCHEDULE_INGEST_BOUNDARY_DATA)
+    },
+    'ingest_building_data': {
+        'task': 'fba.tasks.ingest_building_data',
+        'schedule': crontab(**SCHEDULE_INGEST_BUILDING_DATA)
+    },
+    'ingest_road_data': {
+        'task': 'fba.tasks.ingest_road_data',
+        'schedule': crontab(**SCHEDULE_INGEST_ROAD_DATA)
+    },
+    'ingest_hazard_data': {
+        'task': 'fba.tasks.ingest_hazard_data',
+        'schedule': crontab(**SCHEDULE_INGEST_HAZARD_DATA)
+    }
 }
 
 CELERY_TIMEZONE = 'UTC'
